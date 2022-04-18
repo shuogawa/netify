c710fde422e39d96c7ff7a7421d84b70b2d425aa
shuogawa <shuogawa@gmail.com>
2022-04-18 20:57:54 +0900

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import { Routes, Route, Link } from "react-router-dom";
-import Home from './pages/Home'
-import PageA from './pages/PageA'
-import PageB from './pages/PageB'
-import PageC from './pages/PageC'
-import PageD from './pages/PageD'
-import PageE from './pages/PageE'
-import './App.css';
+import * as React from "react"
+import {
+  ChakraProvider,
+  Box,
+  Text,
+  Link,
+  VStack,
+  Code,
+  Grid,
+  theme,
+} from "@chakra-ui/react"
+import { ColorModeSwitcher } from "./ColorModeSwitcher"
+import { Logo } from "./Logo"
 
-function App() {
-  return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/a" element={<PageA />} />
-        <Route path="/b" element={<PageB />} />
-        <Route path="/c" element={<PageC />} />
-        <Route path="/d" element={<PageD />} />
-        <Route path="/e" element={<PageE />} />
-      </Routes>
-    </div>
-  );
-}
-
-export default App;
+export const App = () => (
+  <ChakraProvider theme={theme}>
+    <Box textAlign="center" fontSize="xl">
+      <Grid minH="100vh" p={3}>
+        <ColorModeSwitcher justifySelf="flex-end" />
+        <VStack spacing={8}>
+          <Logo h="40vmin" pointerEvents="none" />
+          <Text>
+            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
+          </Text>
+          <Link
+            color="teal.500"
+            href="https://chakra-ui.com"
+            fontSize="2xl"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn Chakra
+          </Link>
+        </VStack>
+      </Grid>
+    </Box>
+  </ChakraProvider>
+)
